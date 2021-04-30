f2944f88f165c2fe6bd01052ce709220b0b1438b
Tommi Mäklin <tommi@maklin.fi>
2021-04-30 16:21:14 +0300

    fix cleanup removing the minified css files

--- cleanup.sh
+++ cleanup.sh
@@ -6,8 +6,10 @@ rm deploy.sh add_common_content.sh minify.sh
 rm footer.html header.html navbar.html
 
 # Remove the non-minified files
-rm css/*.css
-rm js/*.js
+rm css/collapsible.css
+rm css/custom-styles.css
+rm js/contact_address.js
+rm js/year.js
 
 # Remove the git repository
 rm -rf .git .gitignore README.md
