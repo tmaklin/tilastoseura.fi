f5c769abba92d269ee1c971647219ddd2c321831
Tommi Mäklin <tommi@maklin.fi>
2021-04-30 16:12:12 +0300

    use tail -r instead of tac for portability

--- add_common_content.sh
+++ add_common_content.sh
@@ -10,8 +10,8 @@
 set -eupo pipefail
 sed -i '/<html lang="fi">/ r header.html' $1
 sed -i '/<body>/ r navbar.html' $1
-tac footer.html > footer_reverse.html
+tail -r footer.html > footer_reverse.html
 tmpname=tmp-$RANDOM
-tac $1 | sed '/<\/body>/ r footer_reverse.html' | tac > $tmpname
+tail -r $1 | sed '/<\/body>/ r footer_reverse.html' | tail -r > $tmpname
 rm footer_reverse.html
 mv $tmpname $1
