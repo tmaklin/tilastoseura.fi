75ed2f02156fcd0b6bc7dfa7f2f83a8d104fc497
Tommi Mäklin <tommi@maklin.fi>
2021-05-03 12:11:23 +0300

    replace nordics with the html entity codes

--- deploy.sh
+++ deploy.sh
@@ -7,23 +7,28 @@ set -eupo pipefail
 
 # Index
 ./add_common_content.sh index.html
+./fix_nordics.sh index.html
 
 # Pages folder
 for page in pages/*.html; do
     ./add_common_content.sh $page
+    ./fix_nordics.sh $page
 done
 
 # Iltapaivaseminaarit subfolder
 for page in pages/iltapaivaseminaarit/*.html; do
     ./add_common_content.sh $page
+    ./fix_nordics.sh $page
 done
 
 # Palkinnot subfolder
 for page in pages/palkinnot/*.html; do
     ./add_common_content.sh $page
+    ./fix_nordics.sh $page
 done
 
 # Tilastopaivat subfolder
 for page in pages/tilastopaivat/*.html; do
     ./add_common_content.sh $page
+    ./fix_nordics.sh $page
 done
