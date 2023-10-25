3d181306c3e75d3309667e48c949d129968a949e
Jyrki Mottonen <jyrki.mottonen@helsinki.fi>
2023-10-25 20:11:03 +0300

    Suomenkieliset ja englanninkieliset sivut

---
+++ add_common_content_en.sh
@@ -0,0 +1,17 @@
+#####
+# Add the common content to a webpage
+# This includes:
+#   headers: header_en.html
+#   navbar: navbar_en.html
+#   footer: footer_en.html
+####
+
+#!/bin/sh
+set -eupo pipefail
+sed -i '/<html lang="fi">/ r header_en.html' $1
+sed -i '/<body>/ r navbar_en.html' $1
+tail -r footer_en.html > footer_reverse.html
+tmpname=tmp-$RANDOM
+tail -r $1 | sed '/<\/body>/ r footer_reverse.html' | tail -r > $tmpname
+rm footer_reverse.html
+mv $tmpname $1
---
+++ cleanup2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eupo pipefail
+
+# Remove the deployment files
+rm deploy.sh add_common_content.sh
+rm footer.html header.html navbar.html
+rm footer_fi.html header_fi.html navbar_fi.html
+rm deploy_en.sh add_common_content_en.sh
+rm deploy_fi.sh add_common_content_fi.sh
+rm footer_en.html header_en.html navbar_en.html
+rm footer_fi.html header_fi.html navbar_fi.html
+
+# Remove the git repository
+rm -rf .git .gitignore README.md
