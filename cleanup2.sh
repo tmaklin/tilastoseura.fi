e7a61b7960fff73717ded2926edcc39e4c41234e
Jyrki Mottonen <jyrki.mottonen@helsinki.fi>
2023-10-26 10:03:51 +0300

    Pieniä muutoksia

--- cleanup2.sh
+++ cleanup2.sh
@@ -4,10 +4,11 @@ set -eupo pipefail
 # Remove the deployment files
 rm deploy.sh add_common_content.sh
 rm footer.html header.html navbar.html
-rm footer_fi.html header_fi.html navbar_fi.html
+
 rm deploy_en.sh add_common_content_en.sh
-rm deploy_fi.sh add_common_content_fi.sh
 rm footer_en.html header_en.html navbar_en.html
+
+rm deploy_fi.sh add_common_content_fi.sh
 rm footer_fi.html header_fi.html navbar_fi.html
 
 # Remove the git repository
